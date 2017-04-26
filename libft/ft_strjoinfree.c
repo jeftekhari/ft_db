8c92cce09046bb04010951fd5a306adebf4e602a
Joe Eftekhari <jeftekha@e1z2r7p26.42.us.org>
2017-04-26 16:02:21 -0700

    libft

---
+++ libft/ft_strjoinfree.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeftekha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/09 13:56:49 by jeftekha          #+#    #+#             */
+/*   Updated: 2017/03/20 14:32:57 by jeftekha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoinfree(char *s1, char *s2, int f)
+{
+	char	*s3;
+
+	if (s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	s3 = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (s3)
+	{
+		ft_strcpy(s3, s1);
+		ft_strcat(s3, s2);
+	}
+	if (f == 1)
+		ft_strdel(&s1);
+	else if (f == 2)
+		ft_strdel(&s2);
+	return (s3);
+}
