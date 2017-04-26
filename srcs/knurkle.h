32c1e005a391c1f9ae5d443587b3ccfe6e0e16c2
Joe Eftekhari <jeftekha@e1z2r7p26.42.us.org>
2017-04-26 16:00:17 -0700

    knurkle.h

---
+++ srcs/knurkle.h
@@ -0,0 +1,26 @@
+#ifndef		KNURKLE_H
+# define		KNURKLE_H
+# include	<sys/types.h>
+# include	<sys/uio.h>
+# include	<unistd.h>
+# include	<stdlib.h>
+# include	<stdio.h>
+# include	<string.h>
+# include	<math.h>
+# include	<conio.h>
+# include	<iostream>
+
+/*
+ *
+ *
+ *
+ *
+ * ROOM FOR PROTOTYPES AND STRUCTS
+ *
+ *
+ *
+ *
+ *
+ * */
+
+# endif
