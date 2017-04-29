9cee6778d6553cf11aebc7b3d300922889df5de6
ssalaues <salimsalaues@gmail.com>
2017-04-28 17:24:17 -0700

    Parsing

--- srcs/readin.c
+++ srcs/readin.c
@@ -1,14 +1,36 @@
 #include "knurkle.h"
-int parse(t_db db)
+int store(t_db db)
 {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (db.db[i])
+    {
+      j = 0;
+      while (db.db[i][j])
+      {
+          k = 0;
+          while (db.db[i][j][k])
+          {
+              //add data
+              //k++;
+          }
+          j++;
+      } 
+      i++;
+    }
     
     return (0);
 }
 int readin(t_db db)
 {
-    FILE    *fp;
+   FILE    *fp;
     
-    if (!(fp = fopen("knurkle.csv", "r")))
-        db_write(db);
+    if (!(fp = fopen("knurkle.csv", "rb")))
+    {
+        printf("Error 52");
+        exit(0);
+    }
     return (0);
 }
