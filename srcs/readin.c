640c0ad1823689a7a108e1b92b8826b51df90bf7
Salim Salaues <ssalaues@student.42.us.org>
2017-05-04 17:04:16 -0700

    Semi working readin

--- srcs/readin.c
+++ srcs/readin.c
@@ -6,7 +6,7 @@
 /*   By: jeftekha <jeftekha@student.42.us.org       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 16:50:28 by jeftekha          #+#    #+#             */
-/*   Updated: 2017/05/04 16:52:31 by jeftekha         ###   ########.fr       */
+/*   Updated: 2017/05/04 16:58:58 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 int readin(t_database db, FILE *fp)
 {
 	char	*line;
+	size_t	len = 1;
 
 	if (!(line = (char*)ft_memalloc(sizeof(char))))
 		wrong(1, db);
 	if (!(fp = fopen("knurkle.csv", "rb")))
 		wrong(3, db);
 	db.row = 0;
-	while (getdelim(&line , (size_t*)100000, ',', fp) && db.row <= 3)
+	while (getdelim(&line , &len, ',', fp) && db.row <= 3)
 		db.db[db.table][db.row++] = strdup(line);
     return (0);
 }
---
+++ srcs/search.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalaues <ssalaues@student.42.us.org       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/04 16:18:27 by ssalaues          #+#    #+#             */
+/*   Updated: 2017/05/04 16:37:51 by ssalaues         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "knurkle.h"
+
+int	search_int(t_database db, char *s)
+{
+}
+
+int	search(t_database db, int v, char *s)
+{
+	int	i = 0;
+	int	j = 0;
+
+	while (db[i][v])
+	{
+		if (strstr(db[i][v],
+	}
+}
