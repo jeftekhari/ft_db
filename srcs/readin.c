33f44f255e772abdd1c8ce323c256496b70dc537
Salim Salaues <ssalaues@student.42.us.org>
2017-05-04 17:51:59 -0700

    Reading functional

--- srcs/readin.c
+++ srcs/readin.c
@@ -6,7 +6,7 @@
 /*   By: jeftekha <jeftekha@student.42.us.org       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 16:50:28 by jeftekha          #+#    #+#             */
-/*   Updated: 2017/05/04 17:32:32 by jeftekha         ###   ########.fr       */
+/*   Updated: 2017/05/04 17:43:27 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 int readin(t_database db, FILE *fp)
 {
 	char	*line;
-	size_t	len = 1;
+	int		b = 1;
+	size_t	len = 100000;
 
 	if (!(line = (char*)ft_memalloc(sizeof(char))))
 		wrong(1, db);
 	if (!(fp = fopen("knurkle.csv", "rb")))
 		wrong(3, db);
-	while (db.db[db.table])
+	while (db.db[db.table] && b > 0)
 	{
 		db.row = 0;
 		while (db.row <= 3)
 		{
-			getdelim(&line , &len, ',', fp);
+			b = getdelim(&line , &len, ',', fp);
 			db.db[db.table][db.row++] = strdup(line);
 		}
 		db.table++;
