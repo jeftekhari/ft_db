3f7dee717216b5e5d4ffbfc66859ae2832ab45e5
Joe Eftekhari <jeftekha@student.42.us.org>
2017-05-04 17:33:44 -0700

    fixed reader

--- srcs/readin.c
+++ srcs/readin.c
@@ -6,7 +6,7 @@
 /*   By: jeftekha <jeftekha@student.42.us.org       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 16:50:28 by jeftekha          #+#    #+#             */
-/*   Updated: 2017/05/04 16:58:58 by ssalaues         ###   ########.fr       */
+/*   Updated: 2017/05/04 17:32:32 by jeftekha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,15 @@ int readin(t_database db, FILE *fp)
 		wrong(1, db);
 	if (!(fp = fopen("knurkle.csv", "rb")))
 		wrong(3, db);
-	db.row = 0;
-	while (getdelim(&line , &len, ',', fp) && db.row <= 3)
-		db.db[db.table][db.row++] = strdup(line);
+	while (db.db[db.table])
+	{
+		db.row = 0;
+		while (db.row <= 3)
+		{
+			getdelim(&line , &len, ',', fp);
+			db.db[db.table][db.row++] = strdup(line);
+		}
+		db.table++;
+	}
     return (0);
 }
