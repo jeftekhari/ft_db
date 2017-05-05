5a9cb42fe2748f660254157aaf124a4042f21286
Salim Salaues <ssalaues@student.42.us.org>
2017-05-04 20:10:00 -0700

    Db print

--- srcs/search.c
+++ srcs/search.c
@@ -6,32 +6,38 @@
 /*   By: ssalaues <ssalaues@student.42.us.org       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 16:18:27 by ssalaues          #+#    #+#             */
-/*   Updated: 2017/05/04 19:43:52 by ssalaues         ###   ########.fr       */
+/*   Updated: 2017/05/04 20:09:18 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "knurkle.h"
 
-int	db_print()
+int	db_print(t_database db, int i, int v)
 {
+	printf("Table ID: %i\n", i); 
+	printf("IDN: %s\n", db.db[i][0]);
+	printf("NAME: %s\n", db.db[i][1]);
+	printf("FAV COLOR: %s\n", db.db[i][2]);
+	printf("GENDER: %s\n", db.db[i][3] );
 	return (0);
 }
 
 int	search(t_database db)
 {
-	int	i = 0;
-	int	j = 0;
+	int		i = 0;
+	char	*s;
 
-	while (db[i])
+	s = search_ask(db);
+	while (db.db[i])
 	{
-		
-		while (db[i][db.type])
+		while (db.db[i][db.type])
 		{
-			if (strstr(db[i][db.type], s))
-				return(print_db(db, i, db.type));
+			if (strstr(db.db[i][db.type], s))
+				return(db_print(db, i, db.type));
 		db.type++;
 		}
 		db.type = 0;
 		i++;
 	}
+	return (0);
 }
