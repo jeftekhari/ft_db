ebc8edac6e7efae90fe6fa84615e40ed2216704c
Joe Eftekhari <jeftekha@student.42.us.org>
2017-05-04 19:12:20 -0700

    search

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jeftekha <jeftekha@student.42.us.org       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 15:59:42 by jeftekha          #+#    #+#             */
-/*   Updated: 2017/05/04 16:46:47 by jeftekha         ###   ########.fr       */
+/*   Updated: 2017/05/04 19:12:04 by jeftekha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ int main()
 			db_write(db, fp);
 		if (strstr("Read", str) || strstr("read", str))
 			readin(db, fp);
-//		if (strstr("Search", str) || strstr("search", str))
-//			search(db, fp);
+		if (strstr("Search", str) || strstr("search", str))
+			search(db, fp);
 		printf("Anything Else? (Y/N):\n");
 		scanf("%s", str);
 		if (strstr("N", str))
