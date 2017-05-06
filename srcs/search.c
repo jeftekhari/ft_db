ee5c89f765d014b810e65012c492cefa3b8eeadf
Salim Salaues <ssalaues@student.42.us.org>
2017-05-05 18:27:11 -0700

    Updating

--- srcs/search.c
+++ srcs/search.c
@@ -6,7 +6,7 @@
 /*   By: ssalaues <ssalaues@student.42.us.org       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 16:18:27 by ssalaues          #+#    #+#             */
-/*   Updated: 2017/05/05 17:32:15 by ssalaues         ###   ########.fr       */
+/*   Updated: 2017/05/05 18:25:53 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,24 +45,33 @@ int		search(t_database db)
 
 t_database	db_update(t_database db)
 {
-	int i = 0;
-	char *s;
+	int		i;
+	char 	*s1;
+	char	*s2;
 
-	s = search_ask(&db, "update");
+	i = 0;
+	s1 = ft_strnew(30);
+	printf("Enter IDN you want to update:\n");
+	scanf("%s", s1);
+	s2 = search_ask(&db, "update to");
 	while (db.db[i])
 	{
-		if (strstr(db.db[i][db.type], s))
+		if (strstr(db.db[i][0], s1))
 		{
 			if (db.type == 0)
-				db.db[i][db.type] = db.idn;
+				db.db[i][db.type] = s2;
 			if (db.type == 1)
-				db.db[i][db.type] = db.name;
+				db.db[i][db.type] = s2;
 			if (db.type == 2)
-				db.db[i][db.type] = db.color;
+				db.db[i][db.type] = s2;
 			if (db.type == 3)
-				db.db[i][db.type] = db.gender;
+				db.db[i][db.type] = s2;
+			free(s1);
+			return (db);
 		}
 		i++;
 	}
+	free(s1);
+	printf("Unknown IDN");
 	return (db);
 }
